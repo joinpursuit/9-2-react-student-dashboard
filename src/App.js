fa0205aa69b79e70ee6eff290eb3320307faf298
Wwright91 <wisdomwright-martin@pursuit.org>
2022-10-29 18:11:48 -0400

    search box added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import data from "./data/data.json";
 import Header from "./Components/Header";
 import GetAllCohorts from "./Components/Cohort";
 import CohortsChange from "./Components/Students";
+import Searchbox from "./Components/Searchbox";
 
 function App() {
   //setting state for the header. the default will be all students upon arrival to the page
   const [title, setTitle] = useState("All Students");
- //setting state for the data shown. the default will be from the data json upon arrival to the page
+  //setting state for the data shown. the default will be from the data json upon arrival to the page
   const [studentData, setStudentData] = useState(data);
+  //setting state for the inputted student
+  const [search, setSearch] = useState("");
+  //setting state for the result of the search
+  const [searchResult, setSearchResult] = useState(data);
 
   return (
     <div>
@@ -19,7 +24,17 @@ function App() {
           setTitle={setTitle}
           setStudentData={setStudentData}
         />
-        <CohortsChange studentData={studentData} title={title} />
+        <div>
+          <br />
+          <Searchbox
+            setStudentData={setStudentData}
+            search={search}
+            setSearch={setSearch}
+            searchResult={searchResult}
+            setSearchResult={setSearchResult}
+          />
+          <CohortsChange studentData={studentData} title={title} />
+        </div>
       </div>
     </div>
   );
