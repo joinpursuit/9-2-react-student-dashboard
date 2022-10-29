e42418dbb5430ddfa10d534b0e5a6131303bbb5f
Bo Latt <bolattny@gmail.com>
2022-10-29 02:16:03 -0400

    add border radius to cards

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,10 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <div className="navbar">
-      <h1>Student Dashboard</h1>
+      <h1>
+        <span className="h1-first-letter">S</span>tudent{" "}
+        <span className="h1-first-letter">D</span>ashboard
+      </h1>
     </div>
   );
 }
