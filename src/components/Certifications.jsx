e2f9f5dcfe77d9d8eb224f95614c320d5907eedb
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-11-10 22:56:17 -0500

    Small bug fixed

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,6 +1,6 @@
 export default function Certifications({certifications}){
-
-    function certifications(student){
+console.log(certifications)
+    function certificationsli(student){
         const {linkedin, resume, github, mockInterview} = student;
         return (<>
             <li><strong>LinkedIn:</strong> {linkedin? "✅" : "❌"}</li>
@@ -10,11 +10,11 @@ export default function Certifications({certifications}){
             </>
         )
     }
-    
+
 return(
 <div>
         <div><h3>Certifications</h3></div>
-        <ul>{certifications(certifications)}</ul>
+        <ul>{certificationsli(certifications)}</ul>
 </div>
 )
 }
